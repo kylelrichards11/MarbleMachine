8c5cac002d200ad2b792070a05e2aa4297ca8e83
Kyle Richards <kyleLRichards11@gmail.com>
2020-06-04 14:19:00 -0400

    Renamed module to machine

--- test_machines.py
+++ test_machines.py
@@ -7,13 +7,13 @@ from components import IO
 from machine import Machine, Connection, SharedLayer
 import custom_machines
 
-def test_module(module, show_all):
+def test_machine(machine, show_all):
     root = Tk()
 
-    module = module(show_all=show_all)
+    machine = machine(show_all=show_all)
 
-    num_inputs = len(module.inputs)
-    num_outputs = len(module.outputs)
+    num_inputs = len(machine.inputs)
+    num_outputs = len(machine.outputs)
 
     inputs = []
     outputs = []
@@ -28,12 +28,12 @@ def test_module(module, show_all):
     
     i = 0
     for input in inputs:
-        m.add_connection(Connection(input, module, comp_to_input=i))
+        m.add_connection(Connection(input, machine, comp_to_input=i))
         i += 1
 
     i = 0
     for output in outputs:
-        m.add_connection(Connection(module, output, comp_from_output=i))
+        m.add_connection(Connection(machine, output, comp_from_output=i))
         i += 1
 
     for comb in itertools.combinations(inputs, 2):
@@ -55,14 +55,14 @@ if __name__ == "__main__":
             module_name = machine
             machine = custom_machines.__all__[machine]
             print(f"{module_name}: show_all={False}")
-            test_module(machine, False)
+            test_machine(machine, False)
             print(f"{module_name}: show_all={True}")
-            test_module(machine, True)
+            test_machine(machine, True)
 
     else:
         machine = custom_machines.__all__[args.machine]
-        # print(f"{args.machine}: show_all={False}")
-        # test_module(machine, False)
+        print(f"{args.machine}: show_all={False}")
+        test_machine(machine, False)
         print(f"{args.machine}: show_all={True}")
-        test_module(machine, True)
+        test_machine(machine, True)
         
